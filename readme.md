4f8a16f02e977db26fa91ef92c297ce139904d2e
Victor Gan <victorgan@gmail.com>
2015-02-20 17:23:54 -0800

    added initial ideas

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Desktop Setup
+By Victor Gan
+https://github.com/victorgan/desktop-setup.git
+
+Notes on setting up my desktop.
+
+## Repositories Used
+https://github.com/victorgan/.vim.git
+
+## Windows
+
+## Ubuntu
+Install:
+
+- Git
+- Matlab
+- Vim Settings
+- Bash settings
+
+See ubuntuProvision.sh
+
---
+++ ubuntuProvision.sh
@@ -0,0 +1,13 @@
+
+echo "Updating apt-get"
+apt-get update
+apt-get upgrade
+
+echo "Installing Git"
+apt-get install git -y > /dev/null # -y yes to all, outputs stream to /dev/null 
+
+echo "Customizing Vim Settings"
+git clone git://github.com/victorgan/.vim.git $HOME/.vim    # copy repository
+echo "source $HOME/.vim/vimrc" > $HOME/.vimrc               # point to repository
+echo "export TERM=xterm-256color" >> $HOME/.bashrc          # add colors
+
