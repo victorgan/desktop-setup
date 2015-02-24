58e29613be35d172be8578a708f4a93903e6a30c
Victor Gan <victorgan@gmail.com>
2015-02-23 20:51:21 -0800

    install vim

--- ubuntuProvision.sh
+++ ubuntuProvision.sh
@@ -1,12 +1,15 @@
 echo "Updating apt-get"
-apt-get update
-apt-get upgrade
+apt-get update > /dev/null 
+apt-get upgrade > /dev/null 
 
 echo "Installing Git"
 apt-get install git -y > /dev/null # -y yes to all, outputs stream to /dev/null 
 
 echo "Installing Emacs"
 apt-get install emacs -y > /dev/null 
+
+echo "Installing Vim"
+apt-get install vim -y > /dev/null 
 
 echo "Customizing Vim Settings"
 git clone git://github.com/victorgan/.vim.git $HOME/.vim    # copy repository
